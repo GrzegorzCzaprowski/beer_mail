6cb9a109d21bc46ca11f8297fb2bb78189f01ac5
Grzegorz Czaprowski <gc@ges.sh>
2019-07-03 11:36:34 +0200

    working db without id autoincrementation

--- db_init.sql
+++ db_init.sql
@@ -1,76 +1,108 @@
-CREATE DATABASE beerdb
--- ************************************** "events"
+CREATE USER testuser
+    WITH PASSWORD 'md599e5ea7a6f7c3269995cba3927fd0093';
 
-CREATE TABLE "events"
-(
- "id"       int NOT NULL,
- "id_users" int NOT NULL,
- "date"     timestamp NOT NULL,
- "place"    text NOT NULL,
- CONSTRAINT "FK_26" FOREIGN KEY ( "id_users" ) REFERENCES "users" ( "id" )
-);
+--
+-- Database creation
+--
 
-CREATE UNIQUE INDEX "PK_events" ON "events"
-(
- "id"
-);
+CREATE DATABASE testdb
+    WITH OWNER testuser;
 
-CREATE INDEX "fkIdx_26" ON "events"
-(
- "id_users"
-);
+--
+--
 
--- ************************************** "guests"
+--
 
-CREATE TABLE "guests"
-(
- "id"        int NOT NULL,
- "id_events" int NOT NULL,
- "id_users"  int NOT NULL,
- "confirm"   boolean NOT NULL,
- CONSTRAINT "FK_34" FOREIGN KEY ( "id_events" ) REFERENCES "events" ( "id" ),
- CONSTRAINT "FK_37" FOREIGN KEY ( "id_users" ) REFERENCES "users" ( "id" )
-);
-
-CREATE UNIQUE INDEX "PK_guests" ON "guests"
-(
- "id"
-);
-
-CREATE INDEX "fkIdx_34" ON "guests"
-(
- "id_events"
-);
-
-CREATE INDEX "fkIdx_37" ON "guests"
-(
- "id_users"
-);
+\connect testdb
 
 -- ************************************** "users"
 
-CREATE TABLE "users"
+-- *************** SqlDBM: PostgreSQL ****************;
+-- ***************************************************;
+
+
+-- ************************************** "public"."users"
+
+CREATE TABLE "public"."users"
 (
- "id"      int NOT NULL,
- "name"    varchar(50) NOT NULL,
- "surname" varchar(50) NOT NULL,
- "email"   varchar(50) NOT NULL
+"id"      int NOT NULL,
+"name"    varchar(50) NOT NULL,
+"surname" varchar(50) NOT NULL,
+"email"   varchar(50) NOT NULL
 
 );
 
-CREATE UNIQUE INDEX "PK_users" ON "users"
+CREATE UNIQUE INDEX "PK_users" ON "public"."users"
 (
- "id"
+"id"
 );
 
 
+ALTER TABLE "public"."users" OWNER TO testuser;
+
+-- *************** SqlDBM: PostgreSQL ****************;
+-- ***************************************************;
 
 
+-- ************************************** "public"."events"
+
+CREATE TABLE "public"."events"
+(
+"id"       int NOT NULL,
+"id_users" int NOT NULL,
+"date"     timestamp NOT NULL,
+"place"    text NOT NULL,
+CONSTRAINT "FK_26" FOREIGN KEY ( "id_users" ) REFERENCES "public"."users" ( "id" )
+);
+
+CREATE UNIQUE INDEX "PK_events" ON "public"."events"
+(
+"id"
+);
+
+CREATE INDEX "fkIdx_26" ON "public"."events"
+(
+"id_users"
+);
+
+ALTER TABLE "public"."events" OWNER TO testuser;
+
+-- *************** SqlDBM: PostgreSQL ****************;
+-- ***************************************************;
 
 
+-- ************************************** "public"."guests"
 
+CREATE TABLE "public"."guests"
+(
+"id"        int NOT NULL,
+"id_events" int NOT NULL,
+"id_users"  int NOT NULL,
+"confirm"   boolean NOT NULL,
+CONSTRAINT "FK_34" FOREIGN KEY ( "id_events" ) REFERENCES "public"."events" ( "id" ),
+CONSTRAINT "FK_37" FOREIGN KEY ( "id_users" ) REFERENCES "public"."users" ( "id" )
+);
 
+CREATE UNIQUE INDEX "PK_guests" ON "public"."guests"
+(
+"id"
+);
 
+CREATE INDEX "fkIdx_34" ON "public"."guests"
+(
+"id_events"
+);
 
+CREATE INDEX "fkIdx_37" ON "public"."guests"
+(
+"id_users"
+);
 
+ALTER TABLE "public"."guests" OWNER TO testuser;
 
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+GRANT ALL ON SCHEMA public TO testuser;
+
+--
