f31d10b21348168a72b940594db78ac2d93d9427
Grzegorz Czaprowski <gc@ges.sh>
2019-07-15 11:44:31 +0200

    fix user delete

--- backend/models/modelsE/delete.go
+++ backend/models/modelsE/delete.go
@@ -7,19 +7,12 @@ func (model EventModel) DeleteEvent(id int) error {
 	if err != nil {
 		return err
 	}
-	numberOfRows, err := res.RowsAffected()
-	if err != nil {
-		return err
-	}
-	if numberOfRows < 1 {
-		return fmt.Errorf("event with id %d dont exists", id)
-	}
 
 	res, err = model.DB.Exec("DELETE FROM events WHERE id=$1", id)
 	if err != nil {
 		return err
 	}
-	numberOfRows, err = res.RowsAffected()
+	numberOfRows, err := res.RowsAffected()
 	if err != nil {
 		return err
 	}
